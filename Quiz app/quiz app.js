534002ff3a862de38a3c9809f762e13a72fdd4ca
Nada Hashem <Nooody2005std@gmail.com>
2026-02-11 08:29:30 +0200

    create bullets for quiz app done

--- Quiz app/quiz app.js
+++ Quiz app/quiz app.js
@@ -1,11 +1,22 @@
+//Create the variables
+let countSpan=document.querySelector('.count span');
+let bulletsContainer=document.querySelector('.bullets .spans')
+
+
+
 function getQuestions () {
   let myRequest = new XMLHttpRequest();
 
   myRequest.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      let questionsObject = JSON.parse(this.responseText);
-      console.log(questionsObject);
+        let questionsObject = JSON.parse(this.responseText);
+        //select questions count
+        let questionsCount=questionsObject.length;
+
+        //Create Bullets + set questions count
+        createBullets(questionsCount);
     }
+
   };
 
   myRequest.open("GET", "html_questions.json", true);
@@ -13,3 +24,22 @@ function getQuestions () {
 }
 
 getQuestions();
+
+
+function createBullets(num){
+    countSpan.innerHTML=num;
+
+    for(let i=0; i<num; i++){
+        bullet=document.createElement('span');
+
+        //Check if the first question
+        if(i == 0){
+            bullet.className="on";
+        }
+
+        //Append bulletSpan in its container
+        bulletsContainer.appendChild(bullet);
+    }
+
+
+}
